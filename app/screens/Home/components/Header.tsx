c7372d3b7e2baf90ca1546a6b9060636d21377be
MatheusBacochina <bacochinayt@hotmail.com>
2023-11-06 18:29:56 -0300

    fixes

--- app/screens/Home/components/Header.tsx
+++ app/screens/Home/components/Header.tsx
@@ -32,26 +32,41 @@ const Header = () => {
         src={"/bg.png"}
         style={{
           objectFit: "cover",
-          transform: `translateY(${((scrolly >= offsetTop ) ? (scrolly - offsetTop) : 0) * 0.5}px)`,
+          transform: `translateY(${
+            (scrolly >= offsetTop ? scrolly - offsetTop : 0) * 0.5
+          }px)`,
         }}
         className="-z-40 opacity-30 md:opacity-40"
       />
-      <div className="container px-4 mx-auto flex items-center sm:items-start flex-col">
-        <Image alt="logo Teem" width={62} height={49} src={"/logo.png"} />
-        <h1 className="text-center sm:text-left text-white mt-[22px] text-[24px] sm:text-[28px] md:text-[31px] lg:text-[33px] xl:text-[35px] 2xl:text-[38px] font-black max-w-[692px] font-roboto uppercase leading-snug tracking-wide">
-          SEJA APROVADO NO TEEM 2024 E AUMENTE EM 60% SUA RENDA MENSAL!
-        </h1>
-        <p className="text-center sm:text-left max-w-2xl mt-3 text-white text-[16px] sm:text-[17px] md:text-[18px] lg:text-[18px] xl:text-[19px] 2xl:text-[20px] font-normal font-roboto leading-normal">
-          Retenha 70% a mais de conteúdo do que qualquer outra metodologia
-          precisando de apenas poucas horas por semana para se preparar.
-        </p>
-        <Link href="#card">
-          <button className="px-9 py-5 w-fit bg-orange-600 rounded-xl hover:bg-orange-400 mt-5 transition-colors flex items-center">
-            <span className="text-xs font-bold font-roboto uppercase text-white">
-              Garanta sua vaga AGORA!
-            </span>
-          </button>
-        </Link>
+      <div className="container mx-auto gap-8 justify-between lg:flex-row flex-col items-center px-4 flex">
+        <div className="xl:basis-[630px] lg:basis-[530px] flex items-center lg:items-start flex-col">
+          <Image alt="logo Teem" width={62} height={49} src={"/logo.png"} />
+          <h1 className="text-center sm:text-left text-white mt-[22px] text-[24px] sm:text-[28px] md:text-[31px] lg:text-[33px] xl:text-[35px] 2xl:text-[38px] font-black max-w-[692px] font-roboto uppercase leading-snug tracking-wide">
+            SEJA APROVADO NO TEEM 2024 E AUMENTE EM 60% SUA RENDA MENSAL!
+          </h1>
+          <p className="text-center sm:text-left max-w-2xl mt-3 text-white text-[16px] sm:text-[17px] md:text-[18px] lg:text-[18px] xl:text-[19px] 2xl:text-[20px] font-normal font-roboto leading-normal">
+            Retenha 70% a mais de conteúdo do que qualquer outra metodologia
+            precisando de apenas poucas horas por semana para se preparar.
+          </p>
+          <Link href="#card">
+            <button className="px-9 py-5 w-fit bg-orange-600 rounded-xl hover:bg-orange-400 mt-5 transition-colors flex items-center">
+              <span className="text-xs font-bold font-roboto uppercase text-white">
+                Garanta sua vaga AGORA!
+              </span>
+            </button>
+          </Link>
+        </div>
+        <div className="aspect-video w-full lg:basis-[600px] 2xl:basis-[700px]">
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/u-XTYYQXa60?si=r_mbvHvwFQ9_ZcNp"
+            title="YouTube video player"
+          
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </header>
   );
--- app/screens/Home/components/Wbnt.tsx
+++ app/screens/Home/components/Wbnt.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 const Wbnt = () => {
   return  <Link href="https://api.whatsapp.com/send/?phone=5511942197690&text=Gostaria+de+falar+com+o+suporte&type=phone_number&app_absent=0">
-      <button className="h-[72px] w-[72px] fixed right-7 bottom-7">
+      <button className="h-[52px] sm:h-[59px] md:h-[63px] lg:h-[67px] xl:h-[71px] 2xl:h-[75px] w-[52px] sm:w-[59px] md:w-[63px] lg:w-[67px] xl:w-[71px] 2xl:w-[75px] fixed right-7 bottom-7">
         <Image alt="logo" style={{fill: 'blue'}} fill src="/wicom.svg" />
       </button>
     </Link>
