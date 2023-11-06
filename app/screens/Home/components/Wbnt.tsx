b86fdd80157c68897b909a49ef65f415d7775eae
MatheusBacochina <bacochinayt@hotmail.com>
2023-11-06 10:31:17 -0300

    change fixed bnt size

--- app/screens/Home/components/Wbnt.tsx
+++ app/screens/Home/components/Wbnt.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 const Wbnt = () => {
   return  <Link href="https://api.whatsapp.com/send/?phone=5511942197690&text=Gostaria+de+falar+com+o+suporte&type=phone_number&app_absent=0">
-      <button className="h-16 w-16 fixed right-7 bottom-7">
+      <button className="h-[72px] w-[72px] fixed right-7 bottom-7">
         <Image alt="logo" fill src="/wicom.svg" />
       </button>
     </Link>
