4b8d29cc6f798b91c430b21bf78d45ce91f4ad73
MatheusBacochina <bacochinayt@hotmail.com>
2023-12-21 10:45:50 -0300

    change wpp link

--- app/screens/Home/components/Wbnt.tsx
+++ app/screens/Home/components/Wbnt.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 const Wbnt = () => {
-  return  <Link href="https://api.whatsapp.com/send/?phone=5511942197690&text=Gostaria+de+falar+com+o+suporte&type=phone_number&app_absent=0">
+  return  <Link href="https://www.grupomedcof.com.br/suporte-whatsapp">
       <button className="h-[52px] sm:h-[59px] md:h-[63px] w-[52px] sm:w-[59px] md:w-[63px]  fixed right-7 bottom-7">
         <Image alt="logo" style={{fill: 'blue'}} fill src="/wicom.svg" />
       </button>
